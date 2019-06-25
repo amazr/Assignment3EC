d28630add1d27c1a3072ca13baf13c61d721abb1
AlexLaptop <alma9011@colorado.edu>
2019-06-24 22:32:07 -0600

    idk wtf is going on

--- RecursiveSieve.cpp
+++ RecursiveSieve.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 void setAr(bool ar[], int L) {
+
 	for (int i = 0; i < L; i++) {
 		if (i < 2) {
 			ar[i] = false;
@@ -12,20 +13,23 @@ void setAr(bool ar[], int L) {
 	}
 }
 
-void findPrimes(bool ar[], int n, int L) {
-
+void findPrimes(bool ar[], int n, int L, int i) {
+	
 	if (ar[n]) {
-		for (int i = n * 2; i < L; i += n) {
+		if (i < L) {
 			ar[i] = false;
+			findPrimes(ar, n, L, i += n);
 		}
 	}
-
+	
 	if (n * n < L) {
-		findPrimes(ar, n + 1, L);
+		i = 2 * n;
+		findPrimes(ar, n + 1, L, i);
 	}
 }
 
 void displayPrimes(bool ar[], int L) {
+
 	for (int i = 0; i < L; i++) {
 		if (ar[i]) {
 			cout << i << " is prime." << endl;
@@ -38,11 +42,12 @@ void displayPrimes(bool ar[], int L) {
 
 
 int main() {
+
 	int L = 100;
 	bool ar[100];
 
 	setAr(ar, L);
-	findPrimes(ar, 2, L);
+	findPrimes(ar, 2, L, 4);
 	displayPrimes(ar, L);
 	
 	return 0;
